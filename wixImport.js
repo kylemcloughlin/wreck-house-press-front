4e70f19dba16f3555cd59763bcd4063daba5c458
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2021-03-01 23:00:33 -0500

    added buttons

---
+++ wixImport.js
@@ -0,0 +1,12 @@
+// export function exportButton_onClick(event) {
+//   let collection = $w("#collectionInput").value;
+
+//   wixData.query(collection)
+//     .find()
+//     .then((results) => {
+//       $w("#textBox").value = JSON.stringify(results.items);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// }
