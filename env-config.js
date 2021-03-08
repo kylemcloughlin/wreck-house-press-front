8361bde85e03b10b551461bb095d075c06b89dd2
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2021-03-07 21:56:32 -0500

    updated env config

--- env-config.js
+++ env-config.js
@@ -1,4 +1,4 @@
 const prod = process.env.NODE_ENV === 'production'
 module.exports = {
-  'process.env.BACKEND_URL': prod ? "http://localhost:3001/": "https://wreck-house-press-back.herokuapp.com'"
+  'process.env.BACKEND_URL': prod ? "https://wreck-house-press-back.herokuapp.com'" : "http://localhost:3001/"
 }
