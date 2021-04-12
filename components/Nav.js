16e1bd0b8b8c07255709985f5f8357d2459212ed
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2021-04-12 04:04:38 -0400

    pdf for download

--- components/Nav.js
+++ components/Nav.js
@@ -98,7 +98,7 @@ const handleToggle = (event) => {
 </div>) : (< Link href="/login"><button className={navStyles.signIn}>Sign In</button></Link>)}
           
         </div>
-
+              {isLoggedIn ? (<div className={navStyles.pdfButton}><a href="/files/download-file.pdf" download="newname" >Download Link</a></div>) : (<div/>)}
       <div className={navStyles.underline}/>
       <ul className={navStyles.navbar}>
       { globalState.map((x, ind)=> {
